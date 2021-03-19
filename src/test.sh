7a25f20f681eaa9da506f33c9319734279db30f3
Jules van der Toorn <J.vanderToorn@student.tudelft.nl>
2021-03-19 12:13:29 +0100

    Added proper train and test params

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,1 @@
-#python3 test.py --exp_num 115 --model_name SocialVRNN --num_test_sequences 100 --scenario real_world/ewap_dataset/seq_eth --record false ;
-#python3 test.py --exp_num 115 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/ewap_dataset/seq_eth --record false ;
-python3 test.py --exp_num 6 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/amsterdam_canals --record true ;
-
-
-#python3 test.py --exp_num 220 --model_name SocialVRNN --num_test_sequences 100 --scenario real_world/ewap_dataset/seq_hotel --record false ;
-#python3 test.py --exp_num 220 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/ewap_dataset/seq_hotel --record true ;
-#
-#python3 test.py --exp_num 101 --model_name SocialVRNN --num_test_sequences 100 --scenario real_world/zara_01 --record false ;
-#python3 test.py --exp_num 101 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/zara_01 --record true ;
-#
-#python3 test.py --exp_num 107 --model_name SocialVRNN --num_test_sequences 100 --scenario real_world/st --record false ;
-#python3 test.py --exp_num 107 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/st --record true ;
-#
-#python3 test.py --exp_num 112 --model_name SocialVRNN --num_test_sequences 100 --scenario real_world/zara_02 --record false ;
-#python3 test.py --exp_num 112 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/zara_02 --record true ;
+python3 test.py --exp_num 300 --model_name SocialVRNN --num_test_sequences 10 --scenario real_world/amsterdam_canals --record true
--- src/train.sh
+++ src/train.sh
@@ -1,1 +1,10 @@
-python3 train.py --exp_num 6 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6;
+echo "Run 1"
+python3 train.py --exp_num 300 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6
+echo "Run 2"
+python3 train.py --exp_num 300 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6
+echo "Run 3"
+python3 train.py --exp_num 300 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6
+echo "Run 4"
+python3 train.py --exp_num 300 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6
+echo "Run 5"
+python3 train.py --exp_num 300 --model_name SocialVRNN --n_mixtures 3 --output_pred_state_dim 4 --scenario real_world/amsterdam_canals --gpu false --prev_horizon 7 --prediction_horizon 20 --dt 1.0 --truncated_backprop_length 20 --others_info relative --pedestrian_vector_dim 4 --batch_size 16 --n_other_agents 6
