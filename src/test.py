d0e1c1020a7fac898ddd421e6d1b67403ea1e4ea
Jules van der Toorn <J.vanderToorn@student.tudelft.nl>
2021-03-19 12:05:44 +0100

    Modified plotter for Google Cloud instance

--- src/test.py
+++ src/test.py
@@ -310,17 +310,13 @@ if test_args.record:
 	recorder = rec(args, data_prep.agent_container.occupancy_grid)
 	if ("real_world" in test_args.scenario) and not test_args.unit_testing:
 		print("Real data!!")
-		# recorder.plot_on_image(input_list, grid_list, all_predictions, y_ground_truth_list, other_agents_list,
-		# 	                       trajectories,test_args)
-		plotargs = (input_list, grid_list, all_predictions, y_ground_truth_list, other_agents_list, trajectories,
-		test_args)
-		Plotter.generate_zoom_plot(recorder.args, *plotargs)
-		# recorder.plot_on_image(input_list, grid_list, all_predictions, y_ground_truth_list,
-		# 					   other_agents_list,
-		# 					   trajectories, test_args, all_traj_likelihood)
-			# recorder.plot_on_video(input_list, grid_list, all_predictions, y_ground_truth_list,
-		#                        other_agents_list,
-		#                        trajectories, all_traj_likelihood, test_args)
+		plotargs = (input_list, grid_list, all_predictions, y_ground_truth_list, other_agents_list, trajectories, test_args)
+		# Generate video
+		try: recorder.plot_on_image(input_list, grid_list, all_predictions, y_ground_truth_list, other_agents_list, trajectories, test_args)
+		except: pass
+		# Generate zoomed-in video
+		try: Plotter.generate_zoom_plot(recorder.args, *plotargs)
+		except: pass
 	else:
 		# recorder.plot_on_image(input_list, grid_list, all_predictions, y_ground_truth_list, other_agents_list,
 		#	                       trajectories,test_args)
